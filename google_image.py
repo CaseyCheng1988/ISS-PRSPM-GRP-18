dd9943ee1495b7f5e9a2995caa672b3c78f5099c
danieltanhx <60249161+danieltanhx@users.noreply.github.com>
2020-08-25 18:19:56 +0800

    Update google_image.py

--- google_image.py
+++ google_image.py
@@ -1,10 +1,10 @@
 # -*- coding: utf-8 -*-
 """
 Created on Tue Aug 18 22:43:20 2020
-
 @author: user
 """
 import time 
+import datetime  
 start=time.perf_counter()
 import pyautogui as pg
 from selenium import webdriver
@@ -16,7 +16,7 @@ prefs = {"profile.managed_default_content_settings.images":2}
 chromeOptions.add_experimental_option("prefs",prefs)
 options = Options()
 options.add_argument('start-maximized') # 
-browser=webdriver.Chrome('C:\\Users\\tanda\\Documents\\python testing\\chromedriver',options=options)  
+browser=webdriver.Chrome('C://Users//user//OneDrive - National University of Singapore//IS5002//projects//chromedriver',options=options)  
 pg.FAILSAFE=True
 
 def Nkey(N,key):
@@ -25,8 +25,7 @@ def Nkey(N,key):
         pg.hotkey(key)
         i+=1
 
-def imageDownload(X,Y,a):
-    pyperclip.copy('NA')
+def imageDownload(X,Y,a,Object):
     time.sleep(2)
     pg.click(x=X, y=Y,button='right')
     time.sleep(2)
@@ -40,9 +39,10 @@ def imageDownload(X,Y,a):
         
     else:
         pg.hotkey('esc')
- 
+    pyperclip.copy(Object)
     time.sleep(4)
     if pg.locateOnScreen(('download.png'),confidence=0.8,region=(11,875,1583,130))!=None:# do download if tally the download.png
+        pg.hotkey('ctrl','v')
         pg.hotkey('enter')
     else: 
         pg.hotkey('esc')
@@ -61,7 +61,7 @@ def pulldown():#pull down the page initially
         count+=1
 
     count1=0
-    while count1<150:
+    while count1<120:
        pg.doubleClick(1906,1010)
        count1+=1
     
@@ -82,21 +82,20 @@ def pullup(n):#pull up to go back the top page
 
 def DownloadLine(i,Object):
     time.sleep(2)
-    Object=Object+str(i)#line location and various points in column
     if i!=0:
         pg.hotkey('esc')
         time.sleep(1)
         pg.doubleClick(1906,937)
         pg.doubleClick(1906,937)
         pg.doubleClick(1906,937)
-        pg.click(1906,178)
-        pg.doubleClick(1906,937)
+        #pg.click(1906,178)
+        pg.click(1906,937)
         
-    imageDownload(228,555,247)
-    imageDownload(562,555,581)
-    imageDownload(978,555,997)
-    imageDownload(1333,555,1352)
-    imageDownload(1711,555,1495)
+    imageDownload(228,555,247,Object+str(datetime.datetime.now().strftime("%H:%M:%S").replace(':',''))+str(i)+'a.jpg')
+    imageDownload(562,555,581,Object+str(datetime.datetime.now().strftime("%H:%M:%S").replace(':',''))+str(i)+'b.jpg')
+    imageDownload(978,555,997,Object+str(datetime.datetime.now().strftime("%H:%M:%S").replace(':',''))+str(i)+'c.jpg')
+    imageDownload(1333,555,1352,Object+str(datetime.datetime.now().strftime("%H:%M:%S").replace(':',''))+str(i)+'d.jpg')
+    imageDownload(1711,555,1495,Object+str(datetime.datetime.now().strftime("%H:%M:%S").replace(':',''))+str(i)+'e.jpg')
     
 def google(Object,line):#browser start all the way until table 
     browser.get('https://www.google.com/imghp?hl=EN')
@@ -109,10 +108,13 @@ def google(Object,line):#browser start all the way until table
     pullup(n)
     i=0
     while i<line:
+        if pg.locateOnScreen('end.png',confidence=0.8)!=None:
+            break
+        
         DownloadLine(i,Object)
         i+=1
     
-google('chicken meat',400)# pls input here. 
+google('raw chicken meat',400)# pls input here. 
 #Object/Chicken Meat is the object is to google search
 #100/line in above means the number of lines needed to download. 
 #For example if 1000 images were to be downloaded, then 200lines has to be inputted in line
